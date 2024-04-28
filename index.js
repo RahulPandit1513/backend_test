3c4072fc7a0ef58f736e64601a2c548b2dc71d00
RahulPandit1513 <rahulpandit1513@gmail.com>
2024-04-28 23:35:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+});
+
+app.get('/home',(req,res)=>{
+    res.send("Hello From HOME PAGE ");
+});
+
+app.get('/about',(req,res)=>{
+    res.send("About Page");
+});
+
+app.get('/contact',(req,res)=>{
+    res.send("Contact Us Page");
+});
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example App listening on port ${PORT}`);
+});
